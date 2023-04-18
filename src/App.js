4a258ed65f7549824bf5335e34885b9759e437ad
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-04-18 12:59:21 +0200

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+  const [state1, setState1] = useState(0);
+  const [state2, setState2] = useState(0);
+  const [state3, setText] = useState(0);
+
+  // useState return two values state which is the stream 
+  const sum = () => {
+    if (state1 && state2 !== '') {
+      const result = parseInt(state1) + parseInt(state2);
+      setText(result);
+    }
+
+
+  }
+  const substract = () => {
+    const result = parseInt(state1) - parseInt(state2);
+    setText(result);
+  }
+  const multiply = () => {
+    const result = parseInt(state1) * parseInt(state2);
+    setText(result);
+  }
+  const division = () => {
+    const result = parseInt(state1) / parseInt(state2);
+    setText(result);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="App-header" >
+        {/* hello world {state} state2 ={state2} */}
+
+        <input type='text' onChange={(e) => setState1(e.target.value)} />
+        <input type='text' onChange={(e) => setState2(e.target.value)} />
+        <button onClick={sum}>+</button >
+        <button onClick={substract}>-</button >
+        <button onClick={multiply}>*</button >
+        <button onClick={division}>/</button >
+
+        <label>{state3}</label>
+      </div>
+
+      {/* <button onClick={() => { state2 += 1; console.log(state2) }} >+</button >
+      <button onClick={() => { console.log(22); setState(state2) }}>-</button> */}
+    </>
   );
 }
 
